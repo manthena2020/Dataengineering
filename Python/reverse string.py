3eb1d7db377a1bbe8c5ca983f83099615d314964
manthena2020 <mramalingaraju3@gmail.com>
2023-03-13 10:32:16 +0530

    adding

---
+++ Python/reverse string.py
@@ -0,0 +1,30 @@
+
+x = input("enter the required string:")
+ip = list(x)
+
+# Create two lists, one for alphabets and one for digits
+alpha= []
+digit = []
+for char in ip:
+    if char.isalpha():
+        alpha.append(char)
+    else:
+        digit.append(char)
+
+# Reverse the list of alphabets
+alpha.reverse()
+
+# Merge the two lists back into a single list
+op = []
+for char in ip:
+    if char.isalpha():
+        op.append(alpha.pop(0))
+    else:
+        op.append(digit.pop(0))
+
+# Convert the list back to a string
+os = ''.join(op)
+
+print(os)
+
+
